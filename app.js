2cd138a287ee77b20b3db22d60e61d89691c8bf9
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-06-18 01:32:41 +0500

    final

---
+++ app.js
@@ -0,0 +1,29 @@
+const bodyElement = document.querySelector("body");
+const soundBtn = document.querySelector("button");
+let isSoundEnabled = false;
+// let sound = new Audio ("./Audio/relaxing-guitar.mp3")
+
+soundBtn.addEventListener("click", () => {
+  sound = new Audio("./Audio/relaxing-guitar.mp3");
+  isSoundEnabled = true;
+  enableSoundBtn.remove();
+});
+
+bodyElement.addEventListener("mousemove", (event) => {
+  if (!isSoundEnabled) return;
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanElement = document.createElement("span");
+  spanElement.style.left = xPos + "px";
+  spanElement.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanElement.style.width = size + "px";
+  spanElement.style.height = size + "px";
+  bodyElement.appendChild(spanElement);
+  sound.currentTime = 0;
+  sound.play();
+  setTimeout(() => {
+    spanElement.remove();
+    sound.remove();
+  }, 2000);
+});
